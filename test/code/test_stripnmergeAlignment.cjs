c3f724976a229bf3fb566187be3202df603bc050
danielc-n <npmdanielc-n@protonmail.com>
2022-11-18 18:04:05 +0100

    full maj : pipelines and transforms can now be required from the package. Constructor modified to be more flexible (use it with brackets'{}'. Added 'justTheBible' to the official pipelines.

---
+++ test/code/test_stripnmergeAlignment.cjs
@@ -0,0 +1,49 @@
+const { PipelineHandler } = require("../../dist/main");
+const { Proskomma } = require("proskomma");
+const fse = require("fs-extra");
+const path = require("path");
+const test = require("tape");
+
+const testGroup = "strip and merge";
+
+const pipelineH = new PipelineHandler({ proskomma: new Proskomma() , verbose: false });
+
+const perfContent = fse.readFileSync(path.resolve(__dirname, "../data/usfms/titus_aligned_eng.json")).toString();
+
+
+let reportStrip = null;
+let outputStrip = null;
+
+test(`strip alignment (${testGroup})`, async (t) => {
+    t.plan(2);
+    try {
+        let output = await pipelineH.runPipeline("stripAlignmentPipeline", {
+            perf: JSON.parse(perfContent)
+        });
+        outputStrip = output.perf;
+        reportStrip = output.strippedAlignment
+        t.ok(outputStrip, "perf alignment stripped");
+        t.ok(reportStrip, "perf report alignement");
+        // await saveFile(JSON.stringify(output.perf, null, 2), "test/outputs/STRIP_perf_titus_stripped_eng.json");
+        // await saveFile(JSON.stringify(output.strippedAlignment, null, 2), "test/outputs/STRIP_strippedAlignment_stripped_eng.json");    
+    } catch (err) {
+        console.log(err);
+        t.fail("stripAlignmentPipeline throws on valid perf");
+    }
+});
+
+test(`merge alignment (${testGroup})`, async (t) => {
+    t.plan(2);
+    try {
+        output = await pipelineH.runPipeline("mergeAlignmentPipeline", {
+            perf: outputStrip,
+            strippedAlignment: reportStrip
+        });
+        t.ok(output, "perf alignment stripped");
+        t.same(output.perf, JSON.parse(perfContent));
+        // await saveFile(JSON.stringify(output.perf, null, 2), "test/outputs/STRIP_perf_titus_merged_align_eng.json");
+    } catch (err) {
+        console.log(err);
+        t.fail("mergeAlignmentPipeline throws on valid perf");
+    }
+});
